f5e170d3da000002ef70533e0b10056bf11a238a
Uriel Shapiro <uriel1999@gmail.com>
2024-10-23 02:33:48 +0300

    Added comments and made improvements in runtime

--- algorithms/Prim.cpp
+++ algorithms/Prim.cpp
@@ -24,54 +24,79 @@ struct prim_vertex
     }
 };
 
+/**
+ * @brief Pop the vertex with the minimum cost from the costs vector
+ * @param costs Vector of costs
+ * @return Vertex with the minimum cost
+ */
 prim_vertex pop_min(std::vector<prim_vertex> &costs)
 {
-    prim_vertex min_cost_pv = costs.front();
-    for (prim_vertex pv : costs)
-    {
-        if (pv.cost < min_cost_pv.cost)
-        {
-            min_cost_pv = pv;
-        }
-    }
-    costs.erase(std::find(costs.begin(), costs.end(), min_cost_pv));
+    // Find the element with the minimum cost
+    auto min_cost_it = std::min_element(costs.begin(), costs.end(),
+                                        [](const prim_vertex &a, const prim_vertex &b)
+                                        {
+                                            return a.cost < b.cost;
+                                        });
+
+    // Extract the minimum cost element
+    prim_vertex min_cost_pv = *min_cost_it;
+
+    // Remove the minimum element from the vector
+    costs.erase(min_cost_it);
     return min_cost_pv;
 }
 
-void modify_neighboors(Graph *g, std::vector<prim_vertex> *costs, prim_vertex pv)
+/**
+ * @brief Modify the costs of the neighbours of the vertex pv
+ * @param g Graph
+ * @param costs Vector of costs
+ * @param pv Vertex to modify the neighbours of
+ */
+void modify_neighbours(Graph *g, std::vector<prim_vertex> &costs, prim_vertex pv)
 {
-    std::list<std::pair<vertex, weight>> neighboors = g->get_neighbors(pv.v);
-    for (auto neighboors_it = neighboors.begin(); neighboors_it != neighboors.end(); neighboors_it++)
+    std::list<std::pair<vertex, weight>> neighbours = g->get_neighbors(pv.v);
+    for (const auto &neighbour : neighbours)
     {
-        for (auto costs_it = costs->begin(); costs_it != costs->end(); costs_it++)
+        auto &neighbour_vertex = neighbour.first;
+        auto weight = neighbour.second;
+
+        // Find the corresponding vertex in the costs vector
+        auto costs_iter = std::find_if(costs.begin(), costs.end(),
+                                       [&](const prim_vertex &c)
+                                       { return c.v == neighbour_vertex; });
+
+        // If the neighbour is found and the current edge weight is less than the stored cost
+        if (costs_iter != costs.end() && costs_iter->cost > weight)
         {
-            if (neighboors_it->first == costs_it->v && costs_it->cost > g->get_weight(neighboors_it->first, pv.v))
-            {
-                costs_it->cost = g->get_weight(neighboors_it->first, pv.v);
-                costs_it->parent = pv.v;
-            }
+            costs_iter->cost = weight;
+            costs_iter->parent = pv.v;
         }
     }
 }
 
+/**
+ * @brief Prim's algorithm to find the minimum spanning tree of a graph
+ * @param g Graph
+ * @return Subgraph containing the minimum spanning tree
+ */
 Subgraph prim(Graph *g)
 {
-
     std::vector<prim_vertex> costs;
     for (vertex v = 0; v < g->get_vertex_count(); v++)
     {
+        // Initialize costs vector with each vertex cost as infinity and parent as max vertex
         costs.push_back(prim_vertex{v, std::numeric_limits<weight>::infinity(), std::numeric_limits<vertex>::max()});
     }
-    Subgraph result(*g);
-    prim_vertex pv = pop_min(costs);
-    result.add_vertex(pv.v);
-    modify_neighboors(g, &costs, pv);
-    while (!costs.empty())
+    Subgraph result(*g);                    // Create a subgraph from the original graph (Initially empty)
+    prim_vertex pv = pop_min(costs);        // Get the vertex with the minimum cost as the start vertex
+    result.add_vertex(pv.v);                // Add the start vertex to the subgraph
+    modify_neighbours(g, costs, pv);        // Modify the costs of the neighbours of the start vertex
+    while (!costs.empty())                  // While not all vertices are added to the subgraph
     {
-        prim_vertex pv = pop_min(costs);
-        result.add_vertex(pv.v);
-        result.add_edge(pv.v, pv.parent);
-        modify_neighboors(g, &costs, pv);
+        prim_vertex pv = pop_min(costs);    // Get the vertex with the minimum cost
+        result.add_vertex(pv.v);            
+        result.add_edge(pv.v, pv.parent);   
+        modify_neighbours(g, costs, pv);    
     }
     return result;
 }
