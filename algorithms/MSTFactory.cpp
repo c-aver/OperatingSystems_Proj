4daac2d80296aff0403b805c82cf20c01f584444
Uriel Shapiro <uriel1999@gmail.com>
2024-10-22 16:34:47 +0300

    Added functionality to the MSTFactory

--- algorithms/MSTFactory.cpp
+++ algorithms/MSTFactory.cpp
@@ -1,10 +1,31 @@
 #include "MSTFactory.hpp"
 #include <string>
+#include <cctype>
+#include <iostream>
+#include <algorithm>
+
+#define DEBUG
 
 using std::string;
-mst_strat_t *mst_strategy(string algo_name) // TODO: parameters
+mst_strat_t *mst_strategy(string algo_name)
 {
-    (void)algo_name;
-    mst_strat_t *result = &kruskal;
-    return result;
+    std::transform(algo_name.begin(), algo_name.end(), algo_name.begin(),
+    [](unsigned char c){ return std::tolower(c); });    // Convert the string to lowercase
+    
+#ifdef DEBUG
+    std::cout << "algo_name: " << algo_name << std::endl;
+#endif
+
+    if(algo_name == "kruskal")
+    {
+        return &kruskal;
+    }
+    // else if(algo_name == "prim")
+    // {
+    //     return &prim;
+    // }
+    else
+    {
+        return nullptr;
+    }
 };
