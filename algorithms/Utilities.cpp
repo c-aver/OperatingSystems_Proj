d373c3029e6ad88b87ea570f614d9217cf9f213e
Oriya Sigawy <Oriyas.07@gmail.com>
2024-10-24 20:24:10 +0300

    doc

--- algorithms/Utilities.cpp
+++ algorithms/Utilities.cpp
@@ -53,15 +53,17 @@ double total_weight(Subgraph *g)
 
 std::pair<double, double> longest_and_farthest(Subgraph *g, vertex v, vertex parent)
 {
-    double farthest = 0.0;
-    double second_farthest = 0.0;
-    double child_longest = 0.0;
+    // Initialize all distances to 0, anything below that is irrelevant
+    double farthest = 0.0; // The farthest descendant from me
+    double second_farthest = 0.0; // The second fathest
+    // A path between them is the longest path that goes through me
+    double child_longest = 0.0; // The longest path known by a child
     for (auto &[u, w] : g->get_neighbors(v))    // Iterate over all of the neighbours of the current vertex
     {
         if (u == parent)
             continue;
         auto [u_longest, u_farthest] = longest_and_farthest(g, u, v);   // Recursively find the longest path from the current vertex to its descendants
-        u_farthest += w;
+        u_farthest += w; // The farthest from me through u is the farthest from u plus the weight of u--v
         if (u_longest > child_longest)
             child_longest = u_longest;
 
@@ -76,45 +78,48 @@ std::pair<double, double> longest_and_farthest(Subgraph *g, vertex v, vertex par
             second_farthest = u_farthest;
         }
     }
-
+    // The longest path I know is either one my child knew (child_longest) or a path between my two farthest descendants
     double longest = std::max({farthest + second_farthest, child_longest});
     return std::make_pair(longest, farthest);
 }
 
 double longest_distance_between_two_vertices(Subgraph *g)
 {
-    return longest_and_farthest(g, 0, g->get_vertex_count()).first;
+    return longest_and_farthest(g, 0, g->get_vertex_count()).first; // 0 is an arbitrary root, first is the longest of longest_and_fathest return pair
 }
 
 double average_distance_between_two_vertices(Subgraph *g)
 {
     // The idea: find how many paths go through each edge. Each edge splits the graph into two parts.
     // The number of paths that go through an edge is the number of vertices in one part times the number of vertices in the other part.
-
-    std::map<vertex, size_t> descendents;
+    std::map<vertex, size_t> descendents; // Remember how many descendants each vertex has (when rooted arbitrarily)
 
     // A recursive lambda function that sets the number of descendents of each vertex
     std::function<size_t(vertex, vertex)> set_descendents = [&descendents, &set_descendents, &g](vertex v, vertex parent)
     {
+        // The first "descendant" is the vertex itself
         descendents[v] = 1;
-        for (auto n : g->get_neighbors(v))
+        for (auto n : g->get_neighbors(v)) // Iterate the neighbors
         {
-            if (n.first == parent)
+            if (n.first == parent) // Ignore the parent
                 continue;
+            // Add to my descendants the descendants of my child
             descendents[v] += set_descendents(n.first, v); // Add the number of descendents of the child
         }
+        // Return to my parent the number of my descendants
         return descendents[v];
     };
 
     set_descendents(g->get_vertices().front(), g->get_vertex_count()); // Start from the first vertex. which has no parent.
 
+    size_t n = g->get_vertex_count();
     double total_weight = 0.0;
     for (auto &[u, v, w] : g->get_edges())
     {
         size_t m = std::min(descendents[u], descendents[v]); // Get the minimum number of descendents that can go through this edge
-        total_weight += m * (g->get_vertex_count() - m) * w; // The amount of paths that go through this edge is m * (n - m)
+        total_weight += m * (n - m) * w; // The amount of paths that go through this edge is m * (n - m)
     }
-    return total_weight / ((g->get_vertex_count() * (g->get_vertex_count() - 1) / 2) + g->get_vertex_count()); // The average distance is the total weight divided by the total number of paths
+    return total_weight / ((n * (n - 1) / 2) + n); // The average distance is the total weight divided by the total number of paths, +n for paths from vertex to itself
 }
 
 double shortest_distance_between_two_vertices(Subgraph *g)
